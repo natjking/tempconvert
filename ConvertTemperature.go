011ce771d1c4e59cea5c28eabba0f2181f01a526
nathking <nathking@uk.ibm.com>
2017-04-03 21:42:43 +0100

    Add travis

--- ConvertTemperature.go
+++ ConvertTemperature.go
@@ -1,22 +1,20 @@
 package tempconvert
 
-import (
-	"math"
-)
+import "math"
 
 func SimpleRound(f float64) float64 {
-	return math.Floor(f + .5)
+        return math.Floor(f + .5)
 }
 
 func Round(f float64, places int) (float64) {
-	shift := math.Pow(10, float64(places))
-	return SimpleRound(f * shift) / shift;
+        shift := math.Pow(10, float64(places))
+        return SimpleRound(f * shift) / shift;
 }
 
 func FahrenheitToCelsius(intemp float64) (float64) {
-	return Round((intemp - 32)*5/9, 5)
+        return Round((intemp - 32)*5/9, 5)
 }
 
 func CelsiusToFahrenheit(intemp float64) (float64) {
-	return Round(intemp*9/5+32, 5)
+        return Round(intemp*9/5+32, 5)
 }
